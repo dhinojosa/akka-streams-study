916ba054328e8a760b231d758973977594382a25
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-10-04 13:19:49 -0400

    Added another case to the SimpleActorScala along with some state.

--- src/main/scala/com/xyzcorp/akka/SimpleActorScala.scala
+++ src/main/scala/com/xyzcorp/akka/SimpleActorScala.scala
@@ -5,9 +5,13 @@ import akka.event.Logging
 
 class SimpleActorScala extends Actor {
   val log = Logging(context.system, this)
-
+  var a = 0
   def receive: PartialFunction[Any, Unit] = {
     case x:String =>
       log.info("received message: " + x + " in Simple Actor Scala")
+      a = a + 2
+    case y:Int =>
+      log.info("received int!")
+      a = a + 4
   }
 }
